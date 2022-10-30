680b2ac534494a6222de5841c451e400f49833e6
IndianaCz <116759609+IndianaCz@users.noreply.github.com>
2022-10-30 15:17:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const title = document.getElementById("ban_title");
+console.log(title);
+title.textContent = "Good Afternoon";
+const subtitle = document.getElementById("ban_sub");
+console.log(subtitle);
+subtitle.textContent = "Hello Patrycja";
